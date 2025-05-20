edfee17ea5642da7b45c3d0ab3fe2b8607c08513
Snakow-creator <mark.politov20070@gmail.com>
2025-05-20 11:02:46 +0300

    v.1.0.0 release

---
+++ secret/config.py
@@ -0,0 +1,5 @@
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.client.bot import DefaultBotProperties
+
+TOKEN = "TOKEN Telegram BOT"
+BOT_PROPERTIES = DefaultBotProperties(parse_mode=ParseMode.HTML)
